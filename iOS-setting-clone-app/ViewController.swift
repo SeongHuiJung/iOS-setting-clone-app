e61ea440d45c54896cf8aa87d495f9acb84eb414
Jseong <tjdgml150237@naver.com>
2024-02-13 14:22:59 +0900

    Initial Commit

---
+++ iOS-setting-clone-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS-setting-clone-app
+//
+//  Created by 정성희 on 2024/02/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
